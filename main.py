5a0241e545919caedbd882d611ce8e445fa77bf0
Harshal0908 <harshal.humane1999@gmail.com>
2021-06-28 01:36:57 +0530

    Creation of Seq2Seq

--- main.py
+++ main.py
@@ -95,3 +95,52 @@ class Decoder(nn.Module):
         # prediction = [batch size, output dim]
 
         return prediction,hidden,cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self,encoder,decoder,device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+        assert encoder.hid_dim == decoder.hid_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.n_layers == decoder.n_layers, \
+            "Encoders and Decoders must have equal Number of layers!"
+
+    def forword(self,src,trg,teacher_forcing_ratio = 0.5):
+        #src = [src_len,batch_size]
+        #trg = [trg_len,batch_size]
+
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        #use english.vocab instead of self.decoder.output_dim
+        trg_vocab_size = self.decoder.output_dim
+
+        #tensor to store decoder outputs
+        outputs = torch.zeros(trg_len,batch_size,trg_vocab_size).to(self.device)
+
+        # last hidden state of the encoder is used as the initial hidden state of the decoder
+        hidden,cell = self.encoder(src)
+
+        #first input to the decoder is <sos> token
+        input = trg[0,:]
+
+        for t in range(1,trg_len):
+
+            # insert input token embedding, previous hidden and previous cell states
+            # receive output tensor (predictions) and new hidden and cell states
+            output, hidden, cell = self.decoder(input,hidden,cell)
+
+            #place prediction in a tensor holding predictions for each token
+            outputs[t] = output
+
+            #get the highest predicted token from our predictions
+            top1 = output.argmax(1)
+
+            teacher_force = random.random() < teacher_forcing_ratio
+
+            input = trg[t] if teacher_force else top1
+
+        return outputs
