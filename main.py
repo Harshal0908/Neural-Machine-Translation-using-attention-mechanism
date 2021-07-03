a6f4fc9d189a6f48c6f89dee2685de638320a37e
Harshal0908 <harshal.humane1999@gmail.com>
2021-07-03 18:26:24 +0530

    Changed the Simple LSTM encoder to a Bidirectional GRU encoder

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 from torchtext.legacy.datasets import Multi30k
 from torchtext.legacy.data import Field, BucketIterator
@@ -25,7 +26,7 @@ def tokenize_de(text):
     """
     Tokenizes German text from a string into a list of strings (tokens) and reverses it
     """
-    return [tok.text for tok in spacy_de.tokenizer(text)][::-1]
+    return [tok.text for tok in spacy_de.tokenizer(text)]
 
 def tokenize_en(text):
     """
@@ -68,17 +69,17 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
 
 #Building an Encoder
 class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
         super().__init__()
         # YAHA SIRF INITIALISATION KIYA HAIN
-        self.hid_dim = hid_dim
-        self.n_layers = n_layers
 
         # "embedding" is an object created for the Layer "Embedding"
         # so to perform embedding process we use its object
         self.embedding = nn.Embedding(input_dim, emb_dim)
 
-        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim,bidirectional=True)
+
+        self.fc = nn.Linear(enc_hid_dim * 2,dec_hid_dim)
 
         self.dropout = nn.Dropout(dropout)
 
@@ -89,16 +90,24 @@ class Encoder(nn.Module):
         embedded = self.dropout(self.embedding(src))
 
         # embedded = [src len, batch size, emb dim]
-        # OUTPUT OF THIS EMBEDDING LAYER IS PASSED ON TO THE LSTM LAYER
-        outputs, (hidden, cell) = self.rnn(embedded)
+        # OUTPUT OF THIS EMBEDDING LAYER IS PASSED ON TO THE GRU LAYER
+        outputs, hidden = self.rnn(embedded)
 
         # outputs = [src len, batch size, hid dim * n directions]
         # hidden = [n layers * n directions, batch size, hid dim]
-        # cell = [n layers * n directions, batch size, hid dim]
 
-        # outputs are always from the top hidden layer
+        # hidden is stacked [forward_1, backward_1, forward_2, backward_2, ...]
+        # outputs are always from the last layer
+        # hidden [-2, :, : ] is the last of the forwards RNN
+        # hidden [-1, :, : ] is the last of the backwards RNN
 
-        return hidden, cell
+        # initial decoder hidden is final hidden state of the forwards and backwards
+        #  encoder RNNs fed through a linear layer
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]),dim=1)))
+        # outputs = [src len, batch size, enc hid dim * 2]
+        # hidden = [batch size, dec hid dim]
+
+        return outputs,hidden
 
 
 class Decoder(nn.Module):
